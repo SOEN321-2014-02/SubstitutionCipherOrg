79cb11bd48a2be833d49a0836b2e287433c3e01c
crazedmodder <ericnbozikian@msn.com>
2014-11-05 11:19:27 -0500

    Added comments Renamed addCharacterAsMostCommon method to set instead of add and renamed an argument

--- monogrammatrix.py
+++ monogrammatrix.py
@@ -1,6 +1,8 @@
 __author__ = 'SOEN321-2014-02 Group: Connor Bode, Sid the Banzai Master, Eric Bozikian, Norman Mirotchnick'
 
 class MonogramMatrix:
+    #Arguments:
+    #	languageFileToBuildFrequencyMatrix is an already opened, readable file object
     def __init__(self, languageFileToBuildFrequencyMatrix):
         self.matrix = {}
         self.learn(languageFileToBuildFrequencyMatrix)
@@ -8,17 +10,20 @@ class MonogramMatrix:
     #Arguments:
     #	languageFileToBuildFrequencyMatrix is an already opened, readable file object
     def learn(self, languageFileToBuildFrequencyMatrix):
-        #read the file by breaking it down by line and then by character
+        #read the file by breaking it down by line
         for line in languageFileToBuildFrequencyMatrix:
+            #now break the line down into individual characters
             for character in line:
-                if character != '\n': #ignore new lines because we do not care about them
+                #ignore new lines because we do not care about them
+                if character != '\n':
                     if character in self.matrix:
                         self.matrix[character] += 1
                     else:
                         self.matrix[character] = 1
 
-    def addCharacterAsMostCommon(self, characterOfSeperator):
-        self.matrix[characterOfSeperator] = 2*self.matrix[max(self.matrix, key=self.matrix.get)]
+    def setCharacterAsMostCommon(self, characterToMakeMostCommon):
+        #Finds the highest value in the dictionary, multiplies it by 2 and then sets the given character to this value
+        self.matrix[characterToMakeMostCommon] = 2*self.matrix[max(self.matrix, key=self.matrix.get)]
 
     def get(self):
         return self.matrix
