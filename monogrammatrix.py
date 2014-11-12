9d15b96121d35b8007c80f3e47531cbdbc9f977d
Eric Bozikian <ericnbozikian@msn.com>
2014-11-11 20:55:44 -0500

    Added the ability to generate a frequency matrix from a cipher text file and then generate a mapping between the language file and cipher file.
    The usage would be:
    -------------------
    Import the monogrammatrix class first:
         from monogrammatrix import MonogramMatrix
    
    Then create an instance of the class with the language file we want to build our frequency matrix from:
         monoMatrixOfLanguageFrequencies = MonogramMatrix('erictest.txt')
    
    If the text file is only words then we have to add spaces as the most common character:
         monoMatrixOfLanguageFrequencies.setCharacterAsMostCommon(' ')
    
    Run the method to map the mono frequencies while passing the cipher text filename as the argument:
         monoMapping = monoMatrixOfLanguageFrequencies.generateMappingBasedOnFrequencies('text_pairs/1.ciphertext.txt')
    
    Create an instance of a guess class with the list of unique characters as the argument:
         newGuess = Guess(monoMatrixOfLanguageFrequencies.getListOfUniqueCharacters())
    
    Assign our mono mapping as the guess
         newGuess.setGuess(monoMapping)

--- monogrammatrix.py
+++ monogrammatrix.py
@@ -5,33 +5,63 @@ class MonogramMatrix:
     #	filenameToBuildFrequencyMatrix is a string representing the filename to use for building the matrix
     def __init__(self, filenameToBuildFrequencyMatrix):
         self.matrix = {}
+        self.uniqueCharactersInFrequencyMatrix = 0
+        self.sumOfFrequencies = 0
         self.__learn(filenameToBuildFrequencyMatrix)
 
     def __learn(self, filenameToBuildFrequencyMatrix):
         with open(filenameToBuildFrequencyMatrix,'r') as languageFileToBuildFrequencyMatrix:
             #read the file by breaking it down by line
             for line in languageFileToBuildFrequencyMatrix:
-                #treat upper and lower case characters as the same
-                line = line.lower()
                 #now break the line down into individual characters
                 for character in line:
                     #ignore new lines because we do not care about them
                     if character != '\n':
+                        #treat upper and lower case characters as the same
+                        if character >= 'A' and character <= 'Z':
+                            character = character.lower()
+                        self.sumOfFrequencies += 1
                         if character in self.matrix:
                             self.matrix[character] += 1
                         else:
                             self.matrix[character] = 1
+                            self.uniqueCharactersInFrequencyMatrix += 1
+
+    def generateMappingBasedOnFrequencies(self, filenameOfCipherText):
+        generatedMapping = {}
+        cipherMonogram = MonogramMatrix(filenameOfCipherText)
+
+        matrixListSorted = sorted(self.matrix, key=self.matrix.get, reverse=True)
+        cipherListSorted = sorted(cipherMonogram.get(), key=cipherMonogram.get().get, reverse=True)
+
+        if len(matrixListSorted) > len(cipherListSorted):
+            endValueForLoop = len(cipherListSorted)
+        else:
+            endValueForLoop = len(matrixListSorted)
+
+        for i in range(0, endValueForLoop):
+            generatedMapping[cipherListSorted[i]] = matrixListSorted[i]
+
+        return generatedMapping
 
     def setCharacterAsMostCommon(self, characterToMakeMostCommon):
-        characterToMakeMostCommon = characterToMakeMostCommon.lower
+        if characterToMakeMostCommon >= 'A' and characterToMakeMostCommon <= 'Z':
+            characterToMakeMostCommon = characterToMakeMostCommon.lower()
         #Finds the highest value in the dictionary, multiplies it by 2 and then sets the given character to this value
         self.matrix[characterToMakeMostCommon] = 2*self.matrix[max(self.matrix, key=self.matrix.get)]
 
+    #Returns the frequencies of characters in the filenameToBuildFrequencyMatrix passed as the constructor argument
     def get(self):
         return self.matrix
+
+    def getNumberOfUniqueCharacters(self):
+        return self.uniqueCharactersInFrequencyMatrix
 
     def getListOfUniqueCharacters(self):
         listOfUniqueCharacters = []
         for key in self.matrix.keys():
             listOfUniqueCharacters.append(key)
         return listOfUniqueCharacters
+
+    def getSumOfFrequencies(self):
+        return self.sumOfFrequencies
