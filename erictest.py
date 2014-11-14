322172609a61ede95474193bcef9f44253bab015
crazedmodder <ericnbozikian@msn.com>
2014-11-14 16:45:15 -0500

    This file contains an example of how to run all the methods/classes I have created.  The latest addition is for the accuracyofdecryption class
    Create an instance of the class with the filename of your dictionary file as an argument:
    accOfDec = AccuracyOfDecryption('filename.txt')
    
    Run the method on the string you want to check the accuracy of and save or use its integer return:
    accurateResultsCount = accOfDec.getCountOfAccurateWords('string to check')

--- erictest.py
+++ erictest.py
@@ -1,5 +1,6 @@
 from guess import Guess
 from monogrammatrix import MonogramMatrix
+from accuracyofdecryption import AccuracyOfDecryption
 
 mono = MonogramMatrix()
 mono.learn_from_file('erictest.txt')
@@ -8,9 +9,13 @@ mono.setCharacterAsMostCommon(' ')
 newGuess = Guess(mono.getListOfUniqueCharacters())
 newGuess.randomGuessOneCharacter()
 
-monoMapping = mono.generateMappingBasedOnFrequencies('text_pairs/1.ciphertext.txt')
-newGuess.setGuess(monoMapping)
-print(monoMapping)
+#monoMapping = mono.generateMappingBasedOnFrequencies('text_pairs/1.ciphertext.txt')
+#newGuess.setGuess(monoMapping)
+#print(monoMapping)
+
+accOfDec = AccuracyOfDecryption('erictest.txt')
+accurateResultsCount = accOfDec.getCountOfAccurateWords('hi my name is eric shady\ncats\tare what?  I taught eminem')
+print(accurateResultsCount)
 
 #print(monoMatrixOfLanguageFrequencies.get())
 #can do a bucket for dictionary?
